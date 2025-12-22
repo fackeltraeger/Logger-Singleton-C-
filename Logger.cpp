67899fb4e82c09d8a26c8f4db1fa74ed2ba2c54a
pavan <pavan.dogiparti@secude.com>
2025-12-22 19:10:52 +0530

    added a logger class and given some instructions in the main function of the cpp program.

--- Logger.cpp
+++ Logger.cpp
@@ -1,8 +1,63 @@
 #include <iostream>
+#include <fstream>
 
 
-int main()
+// added enum class for the select of loglevel like information, debug and error etc.
+enum class LogLevel {
+    INFO,
+    WARNING,
+    ERROR,
+    DEBUG
+};
+
+class Logger {
+private:
+    std::ofstream logFile;
+
+
+    std::string levelToString(LogLevel level) {
+        switch (level) {
+        case LogLevel::INFO:
+            return "INFO";
+        case LogLevel::WARNING:
+            return "WARNING";
+        case LogLevel::ERROR:
+            return "ERROR";
+        case LogLevel::DEBUG:
+            return "DEBUG";
+        }
+        return "LOG-ERROR";
+    }
+
+public:
+
+    void log_filepoener(){
+        logFile.open("app.log", std::ios::app);
+    }
+
+    void log_fileCloser() {
+        if (logFile.is_open())
+            logFile.close();
+    }
+
+    void log(LogLevel level, const std::string& message) {
+        std::string logMessage ="[" +levelToString(level) + "] " + message; //printing this on the debug terminal can be commented if needed can be enabled.
+        std::cout << logMessage << std::endl;
+        if (logFile.is_open())
+            logFile << logMessage << std::endl;
+    }
+};
+
+int main() 
 {
-    std::cout << "sample logger project : Init";
+    Logger logObj1;
+
+    logObj1.log(LogLevel::INFO, "Program started");
+    logObj1.log(LogLevel::DEBUG, "Loading config");
+    logObj1.log(LogLevel::WARNING, "Low memory");
+    logObj1.log(LogLevel::ERROR, "Something failed");
+
     return 0;
 }
+
+
